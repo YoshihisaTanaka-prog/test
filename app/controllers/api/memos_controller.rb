6bdede43700ed291eeb824042895db098730b091
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2020-12-25 13:22:16 +0900

    10th commit

--- app/controllers/api/memos_controller.rb
+++ app/controllers/api/memos_controller.rb
@@ -1,21 +1,25 @@
 class Api::MemosController < ApplicationController
-    skip_before_filter :verify_authenticity_token, only: :create # どうやらこの記述が必要
+    skip_before_action :verify_authenticity_token, only: :create # どうやらこの記述が必要
   
     def create
-      ret = {text: "text"}
-      render :json => ret
-      # memo = Memo.new(create_params)
+      # ret = {text: "text"}
+      # render :json => ret
+      memo = Memo.new(create_params)
   
-      # エラー処理
-      # if memo.save # もし、memoが保存できたら
-      #   memoj = {text: memo.text, check: "check"}
-      #   render :json => memoj
-      # else
-      #   @error_message = [memo.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
-      #   render :json => @error_message
-      # end
+      エラー処理
+      if memo.save # もし、memoが保存できたら
+        memoj = {text: memo.text, check: "check"}
+        render :json => memoj
+      else
+        @error_message = [memo.errors.full_messages].compact # エラーが入ってるインスタンス変数を定義
+        render :json => @error_message
+      end
     end
   
+    def index
+      ret = {text: "text"}
+      render :json => ret
+    end
   
     private
     def create_params
